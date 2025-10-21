dd4e12dbc6101562a79be69cbdcf1c9f1065411b
Lucas Vas <lvas463@gmail.com>
2025-10-21 18:34:13 -0400

    feat: attempt to implement usb interface

---
+++ src/device_handler.rs
@@ -0,0 +1,45 @@
+﻿use core::sync::atomic::{AtomicBool, Ordering};
+use defmt::info;
+use embassy_usb::Handler;
+
+pub struct DeviceHandler {
+    configured: AtomicBool
+}
+
+impl DeviceHandler {
+    pub fn new() -> Self {
+        Self {
+            configured: AtomicBool::new(false)
+        }
+    }
+}
+
+impl Handler for DeviceHandler {
+    fn enabled(&mut self, enabled: bool) {
+        self.configured.store(false, Ordering::Relaxed);
+        if enabled {
+            info!("Device is enabled.");
+        } else {
+            info!("Device is disabled.");
+        }
+    }
+
+    fn reset(&mut self) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("Device is reset.");
+    }
+
+    fn addressed(&mut self, addr: u8) {
+        self.configured.store(false, Ordering::Relaxed);
+        info!("Device is addressed, address set to {}", addr);
+    }
+
+    fn configured(&mut self, configured: bool) {
+        self.configured.store(configured, Ordering::Relaxed);
+        if configured {
+           info!("Device is now configured, it can draw full power");
+        } else {
+            info!("Device is unconfigured, VBUS limit is 100mA");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,41 @@
 #![no_std]
 #![no_main]
 
-use cyw43_pio::{DEFAULT_CLOCK_DIVIDER, PioSpi};
+use cyw43_pio::{PioSpi, DEFAULT_CLOCK_DIVIDER};
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::bind_interrupts;
-use embassy_rp::gpio::{Level, Output};
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::gpio::{Input, Level, Output, Pull};
+use embassy_rp::peripherals::{DMA_CH0, PIO0, USB};
 use embassy_rp::pio::{InterruptHandler, Pio};
+use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
 use embassy_time::{Duration, Timer};
+use embassy_usb::class::hid::{HidReaderWriter, State};
+use embassy_usb::{Builder, Config};
+use usbd_hid::descriptor::{KeyboardReport, SerializedDescriptor};
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
+use crate::device_handler::DeviceHandler;
+
+mod device_handler;
 
 bind_interrupts!(struct Irqs {
     PIO0_IRQ_0 => InterruptHandler<PIO0>;
+    USBCTRL_IRQ => UsbInterruptHandler<USB>;
 });
 
 #[embassy_executor::task]
 async fn cyw43_task(runner: cyw43::Runner<'static, Output<'static>, PioSpi<'static, PIO0, 0, DMA_CH0>>) -> ! {
     runner.run().await
+}
+
+#[embassy_executor::task]
+async fn print_state(mut input: Input<'static>) {
+    loop {
+        input.wait_for_any_edge().await;
+        let state = input.is_high();
+        info!("Pin {} state was changed to {}", input, state);
+    }
 }
 
 #[embassy_executor::main]
@@ -27,12 +44,48 @@ async fn main(spawner: Spawner) {
     let fw = include_bytes!("../firmware/43439A0.bin");
     let clm = include_bytes!("../firmware/43439A0_clm.bin");
 
-    // To make flashing faster for development, you may want to flash the firmwares independently
-    // at hardcoded addresses, instead of baking them into the program with `include_bytes!`:
-    //     probe-rs download ../../cyw43-firmware/43439A0.bin --binary-format bin --chip RP2040 --base-address 0x10100000
-    //     probe-rs download ../../cyw43-firmware/43439A0_clm.bin --binary-format bin --chip RP2040 --base-address 0x10140000
-    //let fw = unsafe { core::slice::from_raw_parts(0x10100000 as *const u8, 230321) };
-    //let clm = unsafe { core::slice::from_raw_parts(0x10140000 as *const u8, 4752) };
+    // Initialize USB HID support
+    let driver = Driver::new(p.USB, Irqs);
+    let mut config = Config::new(0xc0de, 0xcafe);
+    config.manufacturer = Some("BobdaFett");
+    config.product = Some("Testing Flight Controller");
+    config.serial_number = Some("1234567890");
+    config.max_power = 100;
+    config.max_packet_size_0 = 64;
+
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut msos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+    let mut device_handler = DeviceHandler::new();
+
+    let mut state = State::new();
+    let mut builder = Builder::new(
+        driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut msos_descriptor,
+        &mut control_buf
+    );
+
+    builder.handler(&mut device_handler);
+
+    let config = embassy_usb::class::hid::Config {
+        report_descriptor: KeyboardReport::desc(),
+        request_handler: None,
+        poll_ms: 60,
+        max_packet_size: 0,
+    };
+    let hid = HidReaderWriter::<_, 1, 8>::new(&mut builder, &mut state, config);
+
+    let mut usb = builder.build();
+
+    info!("Running USB future.");
+    // After this point, the USB should be connected.
+    let usb_future = usb.run();
+
+    let mut output_pin = Output::new(p.PIN_2, Level::Low);
 
     let pwr = Output::new(p.PIN_23, Level::Low);
     let cs = Output::new(p.PIN_25, Level::High);
@@ -53,6 +106,9 @@ async fn main(spawner: Spawner) {
     let (_net_device, mut control, runner) = cyw43::new(state, pwr, spi, fw).await;
     unwrap!(spawner.spawn(cyw43_task(runner)));
 
+    let test_pin = Input::new(p.PIN_16, Pull::None);
+    unwrap!(spawner.spawn(print_state(test_pin)));
+
     control.init(clm).await;
     control
         .set_power_management(cyw43::PowerManagementMode::PowerSave)
@@ -60,12 +116,14 @@ async fn main(spawner: Spawner) {
 
     let delay = Duration::from_secs(1);
     loop {
-        info!("led on!");
+        info!("leds on!");
         control.gpio_set(0, true).await;
+        output_pin.set_high();
         Timer::after(delay).await;
 
-        info!("led off!");
+        info!("leds off!");
         control.gpio_set(0, false).await;
+        output_pin.set_low();
         Timer::after(delay).await;
     }
 }
